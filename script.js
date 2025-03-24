0208b97c9f373d41a318be3493b8cee57ae56373
danforsomereason <147667936+danforsomereason@users.noreply.github.com>
2025-03-24 13:16:15 -0400

    Updated with UI per Odin Instructions

--- script.js
+++ script.js
@@ -7,57 +7,89 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-function getHumanChoice(roundNumber){
-        
-    while(true){
-        const humanChoice = prompt(`Round ${roundNumber}: Rock, Paper Scissors - Shoot!`).toLowerCase();
-        if(moves.includes(humanChoice)){
-            console.log("Human Choice: ", humanChoice);
-            return humanChoice;
-        }
-        alert('Please enter rock, paper, or scissors');
-    }    
-}
-
+let gameOver = false;
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice){
+let results = document.querySelector(".results");
+
+
+let selection = document.querySelector('.container');
+selection.addEventListener("click", (event) => {
+
+    if(gameOver) return;
+    let humanSelection = null;
+    let target = event.target.textContent.toLowerCase();
+    console.log("Target: ", target);
+
+    switch(target){
+        case 'rock':
+            console.log('Human chooses rock');
+            humanSelection = "rock";
+            break;
+        case 'scissors':
+            console.log('Human chooses scissors');
+            humanSelection = "scissors";
+            break;
+        case 'paper':
+            console.log('Human chooses paper');
+            humanSelection = "paper";
+            break; 
+        default:
+            return;       
+    }
+    if(humanSelection){
+        playRound(humanSelection, getComputerChoice());
+    }    
+})
+
+
+
+// function getHumanChoice(roundNumber){
+        
+//     while(true){
+//         const humanChoice = prompt(`Round ${roundNumber}: Rock, Paper Scissors - Shoot!`).toLowerCase();
+//         if(moves.includes(humanChoice)){
+//             console.log("Human Choice: ", humanChoice);
+//             return humanChoice;
+//         }
+//         alert('Please enter rock, paper, or scissors');
+//     }    
+// }
+
+
+function playRound(humanSelection, computerChoice){
     let message = '';
-    if(humanChoice === 'rock' && computerChoice === 'scissors' ||
-        humanChoice === 'scissors' && computerChoice === 'paper' ||
-        humanChoice === 'paper' && computerChoice === 'rock'){
+    if(humanSelection === 'rock' && computerChoice === 'scissors' ||
+        humanSelection === 'scissors' && computerChoice === 'paper' ||
+        humanSelection === 'paper' && computerChoice === 'rock'){
             humanScore++;
-            message += `You win! ${humanChoice.at(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`;
-        } else if (humanChoice === computerChoice){
-            message += `You draw! You both picked ${humanChoice}.`;
+            message += `You win! ${humanSelection.at(0).toUpperCase() + humanSelection.slice(1)} beats ${computerChoice}.`;
+            results.textContent = message;
+        } else if (humanSelection === computerChoice){
+            message += `You draw! You both picked ${humanSelection}.`;
+            results.textContent = message;
         } else {
             computerScore++;
-            message += `You lose! ${computerChoice.at(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.`;
+            message += `You lose! ${computerChoice.at(0).toUpperCase() + computerChoice.slice(1)} beats ${humanSelection}.`;
+            results.textContent = message;
         }
 
-    console.log('Score - Human: ', humanScore, 'to Computer: ',computerScore);
-    console.log(message);
+        if (humanScore === 3){
+            message = "You win!"
+            results.textContent = message;
+            gameOver = true;
+        } else if (computerScore === 3) {
+            message = "You lose!"
+            gameOver = true;
+            results.textContent = message;
+        }
+
+        console.log('Score - Human: ', humanScore, 'to Computer: ',computerScore);
+        
 }
 
-function playGame(){
-    let gameOver = false
-    let roundNumber = 1;
-    while(!gameOver){
-        const humanSelection = getHumanChoice(roundNumber++);
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        if(humanScore === 3){
-            gameOver = true;
-            return 'You win!';
-            
-        } else if ( computerScore === 3 ){
-            gameOver = true;
-            return 'You lose!';
-            
-        } 
-    }
-    
-}
 
-console.log(playGame());
+
+
+
